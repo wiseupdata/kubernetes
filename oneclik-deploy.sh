a9bdc5267fea4ec789bfa571a3c287b7eab16028
wiseupdata <silvio.liborio@wiseupdata.com>
2023-12-09 10:14:35 +0000

    sync

--- oneclik-deploy.sh
+++ oneclik-deploy.sh
@@ -2,6 +2,6 @@
 git add . && git commit -am "sync" && git push && git tag 0.0.1 -f && git push --tags -f
 
 #new branch
-git checkout -b 0.0.1
+git checkout -b 0.0.1 && git push --set-upstream origin 0.0.1
 
 
