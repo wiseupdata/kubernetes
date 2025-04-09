45ac9bf76e8a28256c153a13bc1a15f6ea1bdf88
silllvio <silvio.84@outlook.com>
2025-04-09 18:38:37 +0100

    sync

--- oneclik-deploy.sh
+++ oneclik-deploy.sh
@@ -1,6 +1,6 @@
 #Deploy on click
-git add . && git commit -am "sync" && git push && git tag 0.0.2 -f && git push --tags -f
+git add . && git commit -am "sync" && git push && git tag 0.0.3 -f && git push --tags -f
 
 #new branch
-git checkout -b 0.0.2 && git push --set-upstream origin 0.0.2
+git checkout -b 0.0.3 && git push --set-upstream origin 0.0.3
 
